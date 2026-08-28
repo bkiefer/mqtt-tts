e2b5a7c9592ea26738cdb9ca3d280ac0d1df5f9f
Bernd Kiefer <j_a_w_bone@gmx.de>
2026-08-28 14:25:45 +0200

    Improve docker build

--- build_docker.sh
+++ build_docker.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+docker images 2>&1 | grep -q mypy:3.11 ||
+    docker build -f Dockerfile_mypy3_11 -t mypy:3.11 .
+
 . utils.sh
 
 ./update_repo.sh
-
-docker build -f Dockerfile_mypy3_11 -t mypy:3.11 .
 
 docker build --no-cache -f Dockerfile -t "$(getimage)" .
